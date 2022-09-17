e0a674b69c376fdb6567cd19bb9b601b49054a82
Omar Sawalmeh <sawalmehomar10@gmail.com>
2022-09-17 21:35:25 +0300

    Setup backend & Connect between backend and frontend

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = () => {
-   const { id } = useParams()
-   const product = products.find((p) => p._id == id)
+  const { id } = useParams()
+  const[product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+      // need to add a proxy='path backend' to cross between the front and backend
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [id])
+
   return (
     <>
       <Link className='btn btn-dark my-3' to='/'>
