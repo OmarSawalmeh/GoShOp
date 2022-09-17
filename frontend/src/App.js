64438da984f4c9d8551fef97296d0ced62e2e48d
Omar Sawalmeh <sawalmehomar10@gmail.com>
2022-09-17 13:02:01 +0300

    Home page && Products details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+
+// components
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+// screen
+import HomeScreen from './screen/HomeScreen'
+import ProductScreen from './screen/ProductScreen'
 
 const App = () => {
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <main className='py-3'>
+         <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact/>
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+         </Container>
+        </main>
+      </BrowserRouter>
+      <Footer />
+    </div>
   )
 }
 
